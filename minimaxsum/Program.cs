66820ca9276a41d23ed48b6600cb4b370e7c047a
Engin Polat <enpolat@microsoft.com>
2020-01-04 22:54:36 +0300

    minimaxsum challenge solution

---
+++ minimaxsum/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace minimaxsum
+{
+    class Program
+    {
+        static void miniMaxSum(long[] arr)
+        {
+            Array.Sort(arr);
+
+            long min = arr.First();
+            long max = arr.Last();
+            long sum = arr.Sum();
+
+            Console.WriteLine((sum - max) + " " + (sum - min));
+        }
+
+        static void Main(string[] args)
+        {
+            long[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt64(arrTemp));
+
+            miniMaxSum(arr);
+        }
+    }
+}
