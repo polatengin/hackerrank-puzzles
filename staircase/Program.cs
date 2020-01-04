1d4fe4bdf03b8545a3d098c21e7b78c463bdf0c4
Engin Polat <enpolat@microsoft.com>
2020-01-04 22:55:03 +0300

    staircase challenge solution

---
+++ staircase/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace staircase
+{
+    class Program
+    {
+        static void staircase(int n) {
+            for(var iLoop = 0; iLoop < n; iLoop++)
+            {
+                Console.WriteLine(new string('#', iLoop + 1).PadLeft(n, ' '));
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            staircase(n);
+        }
+    }
+}
